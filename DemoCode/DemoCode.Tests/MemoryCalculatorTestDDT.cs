d2aad5f80e6c026e145a1db3a40127e2f4d3c539
Andrii Yuskov <a.yuskov@levi9.com>
2016-02-11 17:46:13 +0200

    Before Going to Home

---
+++ DemoCode/DemoCode.Tests/MemoryCalculatorTestDDT.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DemoCode.Tests
+{
+    public class MemoryCalculatorTestDDT
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public MemoryCalculatorTestDDT(ITestOutputHelper helper)
+        {
+            _outputHelper = helper;
+        }
+
+        [Theory]
+        [InlineData(2,4,-6)]
+        [InlineData(-10,-5,15)]
+        [InlineData(10,0,-10)]
+        public void ShouldSubtractTwoNumbers(int first, int second, int expected)
+        {
+            var sut = new MemoryCalculator();
+            sut.Subtract(first);
+            sut.Subtract(second);
+            
+            Assert.Equal(expected, sut.CurrentValue);
+
+        }
+
+    }
+}
