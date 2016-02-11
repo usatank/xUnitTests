142ec73fe5431304e3638046d5d81f6951fcb53e
Andrii Yuskov <a.yuskov@levi9.com>
2016-02-11 14:31:32 +0200

    Adding Single Test class

---
+++ DemoCode/DemoCode.Tests/MemoryCalculatorTestsSingleInstance.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DemoCode.Tests
+{
+    public class MemoryCalculatorFixture : IDisposable
+    {
+        public MemoryCalculator Sut { get; private set; }
+
+        public MemoryCalculatorFixture()
+        {
+            this.Sut = new MemoryCalculator();
+            
+        }
+
+        public void Dispose()
+        {
+            this.Sut.Dispose();
+        }
+    }
+
+    [Trait("Category", "Memory Calculator Test Single Instance")]
+    public class MemoryCalculatorTestsSingleInstance : IClassFixture<MemoryCalculatorFixture>
+    {
+        private readonly ITestOutputHelper _testOutput;
+        private readonly MemoryCalculatorFixture _fixture;
+
+        
+        public MemoryCalculatorTestsSingleInstance(ITestOutputHelper testOutput, MemoryCalculatorFixture fixture)
+        {
+            this._testOutput = testOutput;
+            this._fixture = fixture;
+            this._fixture.Sut.Clear();
+        }
+
+        [Fact]
+        public void ShouldAdd()
+        {
+            
+            _fixture.Sut.Add(10);
+            _fixture.Sut.Add(5);
+
+            Assert.Equal(15, _fixture.Sut.CurrentValue);
+        }
+
+        [Fact]
+        public void ShouldSubtract()
+          {
+            
+            _fixture.Sut.Subtract(3);
+            _fixture.Sut.Subtract(4);
+            Assert.Equal(-7, _fixture.Sut.CurrentValue);
+
+        }
+
+    }
+}
